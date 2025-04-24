d4955c2c48336271870fec1ec9c773e5e9702fa1
Panic <smusenko.77@gmail.com>
2025-04-24 20:27:46 +0200

    1s rewiew

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard({ name, link }) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData);
+ cardsContainer.append(card);
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => {
+    card.remove();
+  });
+});
+
+
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
